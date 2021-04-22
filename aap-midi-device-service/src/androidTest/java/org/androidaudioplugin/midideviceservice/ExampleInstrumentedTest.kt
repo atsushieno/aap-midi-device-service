72d6d208badf6ce0d861e4c6307da2bb28529556
atsushieno <atsushieno@gmail.com>
2021-04-23 01:34:10 +0900

    Add instrumented test for some sanity check.

--- aap-midi-device-service/src/androidTest/java/org/androidaudioplugin/midideviceservice/ExampleInstrumentedTest.kt
+++ aap-midi-device-service/src/androidTest/java/org/androidaudioplugin/midideviceservice/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package org.androidaudioplugin.midideviceservice
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.androidaudioplugin.PluginInformation
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,5 +21,18 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("org.androidaudioplugin.aap_midi_device_service", appContext.packageName)
+
+        applicationContextForModel = appContext
+        val model = ApplicationModel(appContext.packageName, appContext)
+
+        val plugins = org.androidaudioplugin.midideviceservice.model.pluginServices.flatMap { s -> s.plugins }.toList()
+            .filter { p -> p.category?.contains("Instrument") ?: false || p.category?.contains("Synth") ?: false }
+
+        for (plugin in plugins) {
+            model.specifiedInstrument = plugin
+            model.initializeMidi()
+            model.playNote()
+            model.terminateMidi()
+        }
     }
 }
